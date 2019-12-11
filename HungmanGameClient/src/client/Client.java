6c8275c2dde7bce515b894a0f48f4de5de9c6268
diana_fedotova <diana.fedotova.2000@mail.ru>
2019-12-11 13:51:31 +0300

    reformed client and server

--- HungmanGameClient/src/client/Client.java
+++ HungmanGameClient/src/client/Client.java
@@ -1,4 +1,4 @@
-package game;
+package client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- HungmanGameClient/src/client/Controller.java
+++ HungmanGameClient/src/client/Controller.java
@@ -1,4 +1,4 @@
-package game;
+package client;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
